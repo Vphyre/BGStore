be71cf155b39ab846e3847852b8f2dd96a01c2d2
vphyre <vinipaulnic@hotmail.com>
2024-05-15 20:58:06 -0300

    Adding Basic Player Movement

---
+++ BGStore/Assets/Scripts/Input/PlayerMove.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private PlayerInputs controls;
+    private Rigidbody2D rigidBody;
+
+    private void Awake()
+    {
+        controls = new PlayerInputs();
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+    private void FixedUpdate()
+    {
+        Vector2 moveInput = controls.Main.Movement.ReadValue<Vector2>();
+        rigidBody.velocity = moveInput * _speed;
+    }
+}
